1974775e2153f487d3cb43075433611bc0dd3e12
rujjal <rujjalsada25@gmail.com>
2021-06-24 09:19:49 +0545

    creating base

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,16 @@
 
 from django.urls import path
-from .views import home
+from .views import *
 
 app_name = "home"
+
+
 urlpatterns = [
     path('', home, name='home'),
+#    path('about', about, name='about'),
+#    path('contact', contact, name='contact'),
+#    path('portfolio', portfolio, name='portfolio'),
+#    path('price', price, name='price'),
+#    path('services', services, name='services'),
+   
 ]
