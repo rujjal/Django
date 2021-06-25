384b8a8897555b041d69ae5571ccd7301739f8c6
rujjal <rujjalsada25@gmail.com>
2021-06-25 16:08:29 +0545

    models created

--- home/views.py
+++ home/views.py
@@ -2,20 +2,36 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
-def home(request):
+
+def views():
 	view = {}
 	view['feedback'] = Feedback.objects.all()
+	return view
 
+def home(request):
+	view = views()
 	return render(request,'index.html',view)
 
-
-
 def about(request):
-	return render(request,'about.html')
-
+	view = views()
+	return render(request,'about.html',view)
 
 def contact(request):
-	return render(request,'contact.html')
+	status = {}
+	if request.method == 'POST':
+		name = request.POST['name']
+		email = request.POST['email']
+		subject = request.POST['subject']
+		message = request.POST['message']
+		data = Contact.objects.create(
+			name = name,
+			email = email,
+			subject = subject,
+			message = message
+			)
+		data.save()	
+		status['message'] = 'Form is submitted.'
+	return render(request,'contact.html',status)
 
 
 def portfolio(request):
