1e2e473db361f4e90e80124157413e7317b24946
rujjal <rujjalsada25@gmail.com>
2021-06-25 12:59:36 +0545

    creating models

--- home/views.py
+++ home/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-	return render(request,'index.html')
+	view = {}
+	view['feedback'] = Feedback.objects.all()
+
+	return render(request,'index.html',view)
+
+
+
+def about(request):
+	return render(request,'about.html')
+
+
+def contact(request):
+	return render(request,'contact.html')
+
+
+def portfolio(request):
+	return render(request,'portfolio.html')
+
+
+def price(request):
+	return render(request,'price.html')
+
+
+def services(request):
+	return render(request,'services.html')	
